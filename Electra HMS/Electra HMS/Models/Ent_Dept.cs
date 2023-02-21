f14641410a776f9a9684c031acce7b33d1dcffe6
Sarath-STS <saraththynakathoot@gmail.com>
2023-02-21 11:06:53 +0530

    Ent Model changed

---
+++ Electra HMS/Electra HMS/Models/Ent_Dept.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Electra_HMS.Models
+{
+    public class Ent_Dept
+    {
+        [Key]
+        public int DeptId { get; set; }
+
+        [Required(ErrorMessage = "Required!!")]
+        [DisplayName("Department Name")]
+        public string DeptName { get; set; }
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+        [DisplayName("Status")]
+        public string Status { get; set; }
+    }
+}
---
+++ Electra HMS/Electra HMS/Models/Ent_Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Electra_HMS.Models
+{
+    public class Ent_Login
+    {
+        [ScaffoldColumn(false)]
+        public string LoginId { get; set; }
+
+        [Required(ErrorMessage = "Required!!")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail adress")]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Password")]
+        public string Password { get; set; }
+    }
+}
