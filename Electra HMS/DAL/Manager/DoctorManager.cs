34abb127940fd78009d46ab7d3c0fc55e15b4f7c
Sarath-STS <saraththynakathoot@gmail.com>
2023-03-15 10:20:03 +0530

    Manager

--- Electra HMS/DAL/Manager/DoctorManager.cs
+++ Electra HMS/DAL/Manager/DoctorManager.cs
@@ -11,6 +11,15 @@ namespace DAL.Manager
     public class DoctorManager
     {
         Model1 db = new Model1();
+        public List<Doctor> DoctorsList()
+        {
+            return db.Doctor.Where(e => e.D_Status == "A").ToList();
+        }
+
+        public Doctor IsExistEmail(string emailId)
+        {
+            return db.Doctor.Where(e => e.D_Email == emailId).SingleOrDefault();
+        }
     }
 
 }
